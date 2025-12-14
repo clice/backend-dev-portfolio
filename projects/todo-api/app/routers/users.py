390ffc3e782901b913e60a6f6808f643c031b17b
Clice Romão <clice.bbr@gmail.com>
2025-12-14 18:45:02 -0300

    feat: remover to-do list

--- projects/todo-api/app/routers/users.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, HTTPException
-from sqlalchemy.orm import Session
-
-from app.schemas.user import UserCreate, UserResponse
-from app.models.user import User
-from app.database.session import SessionLocal
-from app.core.security import hash_password
-
-router = APIRouter(prefix="/users", tags=["Users"])
